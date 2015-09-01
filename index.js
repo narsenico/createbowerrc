5c7a8a8d793c445950852d1f3b5605c92e9b0783
narsenico <not_amon-dev@yahoo.it>
2015-09-01 16:52:00 +0200

    parse args

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 /* createbowerrc
  * ver. 0.0.1
  * by narsenico
+ *
+ * args: 
+ * -d|-directory <path>
  */
 var fs = require('fs');
 var process = require('process');
 //regex per le chiavi inerenti al proxy 
 var KEYS = /^(https\-proxy|proxy)=(.*)/;
+var ARG_DIRECTORY = /^\-(directory|d)$/;
+var ARG_STRICTSSL = /^\-(s|ssl)$/;
 
 function getUserHome() {
     return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
-exports.create = function(directory) {
+function parseArgv(argv) {
+    var args = {};
+    for (var ii=2; ii<argv.length; ii++) {
+        if (ARG_DIRECTORY.test(argv[ii])) {
+            args.directory = argv[++ii];
+        } else if (ARG_STRICTSSL.test(argv[ii])) {
+            args.strictssl = (argv[++ii] === 'true');
+        }
+    }
+    return args;
+}
+
+exports.create = function() {
+    var args = parseArgv(process.argv);
+    console.log(args);
+
+    //TODO scrivere nel file .bowerrc anche i parametri recuperati da riga di comando
+
     //leggo il contenuto del file di configurazione di npm
     //e recupero le impostazioni del proxy
     var path = getUserHome() + '/.npmrc';
